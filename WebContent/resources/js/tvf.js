e7da2d6f3a2bdeeb90e1ee52d05e0d10d84080ef
okskmk2 <okskmk2@naver.com>
2017-07-22 06:54:05 +0900

    Update tvf.js
    사용하지 않는 함수 삭제

--- WebContent/resources/js/tvf.js
+++ WebContent/resources/js/tvf.js
@@ -1,19 +1,4 @@
 function toggleBar(target) {
 	document.getElementById("header-nav").classList.toggle("off");
 }
-function showBar(target) {
-	document.getElementById(target).style.display = "flex";
-	document.getElementById(target).style.flexDirection = "column";
-}
-function hideBar(target) {
-	document.getElementById(target).style.display = "none";
-}
-function resize() {
-	if (document.body.clientWidth > 720) {
-		//showBar("header-nav");
-
-	} else {
-		//hideBar("header-nav");
-	}
-}
 
